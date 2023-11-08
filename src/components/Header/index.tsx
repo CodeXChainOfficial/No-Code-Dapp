57c69ea23be3e73c906f4d0719119c180c62bd0f
CodeXChainOfficial <wefundofficial@gmail.com>
2023-11-08 18:24:32 +0800

    pop up

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,17 +10,7 @@ type HeaderProps = React.DetailedHTMLProps<
 
 const Header: React.FC<HeaderProps> = (props) => {
 
-  const openPopup = () => {
-    const popupWindow = window.open('http://127.0.0.1:8000/?canisterId=bkyz2-fmaaa-aaaaa-qaaaq-cai', 'Connect2IC Popup', 'width=600,height=400');
-  
-    // You can optionally focus the new window
-    if (popupWindow) {
-      popupWindow.focus();
-    }
-  };
-  
-  // Call this function when you want to open the popup
-  openPopup();
+ 
 
   return (
     <>
@@ -74,7 +64,6 @@ const Header: React.FC<HeaderProps> = (props) => {
             </li>
           </ul>
           <Button className="bg-gradient1  capitalize cursor-pointer font-raleway font-semibold min-w-[160px] py-[11px] rounded text-base text-center text-white-A700 tracking-[0.80px] hover:bg-green-400_7f"
-          onClick={openPopup}
 
           >
             connect wallet
